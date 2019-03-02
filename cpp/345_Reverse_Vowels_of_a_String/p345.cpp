1efc953e7df1b2c7b719c95b6512261262997585
jinshendan <jinshendan@gmail.com>
2019-03-03 00:22:23 +0100

    add solution #345

---
+++ cpp/345_Reverse_Vowels_of_a_String/p345.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    string reverseVowels(string s) {
+        vector<int> index;
+        set<char> voy = {'a','e','i','o','u','A','E','I','O','U'};
+        for (int i = 0; i < s.length(); i++)
+            if (voy.count(s[i])) index.push_back(i);
+        int n = index.size();
+        for (int i = 0; i < n / 2; i++){
+            char tmp = s[index[i]];
+            s[index[i]] = s[index[n-1-i]];
+            s[index[n-1-i]] = tmp;
+        }            
+        return s;
+    }
+};
